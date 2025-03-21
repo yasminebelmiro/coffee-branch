fc09d8361909b2f8f392c8732ea0ebaa2c4d3287
YasmineBelmiro <devyasminebelmiro@gmail.com>
2025-03-20 21:00:24 -0300

    feat: estruturando o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Search from "./pages/Search/Search";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ src/global.jsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+padding: 0;
+margin: 0;
+box-sizing: border-box;
+}
+
+body{
+width: 100wh;
+height: 100vh;
+background-color: #805731;
+}
+`;
