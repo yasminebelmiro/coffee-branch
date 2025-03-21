ea56cc52c383be179f3e006aa4179cb8dc8455e3
YasmineBelmiro <devyasminebelmiro@gmail.com>
2025-03-20 21:03:06 -0300

    feat: add lista de produtos

---
+++ src/components/ProductList/style.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Separator = styled.div`
+  width: 100%;
+  height: 3rem;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+`;
+
+export const SeparatorTitle = styled.h1`
+  padding-left: 2rem;
+  font-size: 1rem;
+  font-family: "Major Mono Display", monospace;
+`;
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap:1rem;
+   padding: 2rem;
+`;
+
+export const ProductListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+`;
