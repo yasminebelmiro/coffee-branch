72dba6a296d92662f80db9b9086e1d54638f1cc3
YasmineBelmiro <devyasminebelmiro@gmail.com>
2025-03-27 07:12:20 -0300

    feat: cadastro com o google

---
+++ src/service/firebaseConfig.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2y8Y1IlQSgvCPQ97DkagAYAz70336TyQ",
+  authDomain: "coffee-branch-bbddc.firebaseapp.com",
+  projectId: "coffee-branch-bbddc",
+  storageBucket: "coffee-branch-bbddc.firebasestorage.app",
+  messagingSenderId: "61837563221",
+  appId: "1:61837563221:web:67505af3fda2cfb11ccbdc",
+  measurementId: "G-J90LY0SC87"
+};
+
+
+export const app = initializeApp(firebaseConfig);
